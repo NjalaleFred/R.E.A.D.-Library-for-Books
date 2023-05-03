7143cadca2dd7eec6a17c2b72692b4bf8af8086f
ryan <ryan.kimani0@gmail.com>
2023-05-03 12:06:37 +0300

    Search bar Added
    Co-authored-by: Ryan Kimani <ryan.kimani0@gmail.com>

---
+++ Components/.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+ 
+const SearchBar= ({handleSearch})=> {
+    const [searchQuery, setSearchQuery] = useState("")
+    const handleInputChange = (event) => {
+        setSearchQuery(event.target.value)
+    }
+    return (
+        <div className="search-bar">
+            <input
+            type='text'
+            placeholder='Enter the name of the book'
+            value={searchQuery}
+            onChange={handleInputChange}
+            />
+        </div>
+    )
+}
+    
+
+export const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+
+
--- Components/Search.js
+++ Components/Search.js
@@ -1,3 +1,10 @@
+import React,{use} from 'react'
+
+export const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
 
 
 
