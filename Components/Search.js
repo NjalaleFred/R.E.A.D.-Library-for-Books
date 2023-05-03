cb2b4d303ff386a7ab44e8753bb22434d358ab3f
ryan <ryan.kimani0@gmail.com>
2023-05-03 12:44:15 +0300

    Created Search button Component
    co-authored-by: Ryan Kimani <ryan.kimani0@gmail.com>

--- Components/Search.js
+++ Components/Search.js
@@ -1,10 +1,26 @@
-import React,{use} from 'react'
-
-export const Search = () => {
-  return (
-    <div>Search</div>
-  )
+import React,{useState} from 'react'
+ 
+const SearchBar= ({handleSearch})=> {
+    const [searchQuery, setSearchQuery] = useState("")
+    const handleInputChange = (event) => {
+        setSearchQuery(event.target.value)
+    }
+    return (
+        <div className="search-bar">
+            <input
+            type='text'
+            placeholder='Enter the name of the book'
+            value={searchQuery}
+            onChange={handleInputChange}
+            />
+            <SearchButton handleSearch={() =>
+               handleSearch(searchQuery)}/>
+        </div>
+    )
 }
+    
+
+export default SearchBar
 
 
 
