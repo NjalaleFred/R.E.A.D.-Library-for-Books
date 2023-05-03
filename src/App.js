444eb051060ec29ae99ec87cf266d26cd2c7951c
Njalale <fnjalale@gmail.com>
2023-05-03 15:38:43 +0300

    Created Add book and DisplayBook components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
+import AddBook from "./Components/AddBook";
+import { DisplayAddedBook } from "./Components/DisplayAddedBook";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4001/books", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => setBooks(data));
+  }, []);
+
+  function deleteBook(id) {
+    const updatedBooks = books.filter((book) => book.id !== id);
+    setBooks(updatedBooks);
+  }
+
   return (
     <div className="App">
-    
+      <DisplayAddedBook
+        books={books}
+        setBooks={setBooks}
+        onDeleteBook={deleteBook}
+        id={books.id}
+      />
+      <AddBook books={books} setBooks={setBooks} />
     </div>
   );
 }
---
+++ src/Components/AddBook.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function AddBook({books, setBooks}) {
+  
+}
+
+export default AddBook;
