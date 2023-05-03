1823aaad0901f7cdca524adfad8f215974c6a541
Njalale <fnjalale@gmail.com>
2023-05-03 15:40:22 +0300

    Display Books edited

--- src/Components/DisplayAddedBook.js
+++ src/Components/DisplayAddedBook.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 export const DisplayAddedBook = ({ books, onDeleteBook }) => {
+  function handleDeleteBook(id) {
+    fetch(`http://localhost:4001/books/${id}`, {
+      method: "DELETE",
+    }).then(() => onDeleteBook(id));
+  }
 
+  const myBooks = books.map((book) => {
+    return (
+      <div key={book.id}>
+        <h2>{book.title} </h2>
+        <h4>{book.author} </h4>
+        <img src={book.image} alt={book.title}></img>
+        <button onClick={() => handleDeleteBook(book.id)}>Delete Book</button>
+      </div>
+    );
+  });
 
-  return <div></div>;
+  return <div>{myBooks}</div>;
 };
