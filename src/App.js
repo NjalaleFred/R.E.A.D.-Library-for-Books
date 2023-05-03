888a4efc0f4e84b6b97d797d58bdd57fb4ff3903
Njalale <fnjalale@gmail.com>
2023-05-03 16:14:56 +0300

    Added fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [books, setBooks] =useState([])
+  useEffect(() => {
+    fetch(
+      `https://gutendex.com/books/?fields=id,title,authors,description,image,subjects`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data.results);
+      });
+  }, []);
+
   return (
     <div className="App">
     
