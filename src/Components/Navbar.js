05d135e526f111aac5ea33d7434f78405ca5dfd0
Njalale <fnjalale@gmail.com>
2023-05-04 16:01:33 +0300

    Done navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export const NavBar = () => {
+  return (
+    <nav>
+      <NavLink to="/" className="link">
+        Home
+      </NavLink>
+      <NavLink to="/borrow" className="link">
+        Borrow a book
+      </NavLink>
+      <NavLink to="/add" className="link">
+        Add a book
+      </NavLink>
+    </nav>
+  );
+};
