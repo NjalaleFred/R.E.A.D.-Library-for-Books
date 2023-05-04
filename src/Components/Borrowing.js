b24dd7516e0b8bc8aae86e51838aa0597f5d77ef
Alex Njagi <1992anjagi@gmail.com>
2023-05-04 07:32:05 +0300

    Document elements created
    co-authored-by Alex-Njagi <1992anjagi@gmail.com>

--- src/Components/Borrowing.js
+++ src/Components/Borrowing.js
@@ -6,5 +6,70 @@ export function Borrowing ({books}) {
     const today = new Date();
     const currentDate=today.getDate() + "/"+ parseInt(today.getMonth()+1) +"/"+today.getFullYear();       
     const dueDate=((today.getDate())+3) + "/"+ parseInt(today.getMonth()+1) +"/"+today.getFullYear();
-    console.log(dueDate);
+
+    return (
+        <div style={{border: 'solid', borderColor: 'black'}}>
+            <header style={{
+                    backgroundColor: 'whitesmoke',
+                    padding: '20px',
+                    border: 'solid',
+                    borderColor: 'gray'
+                    }}>
+                "Borrow A Book"
+            </header>
+
+            <div>
+                <header style={{
+                    backgroundColor: 'gray',
+                    padding: '5px',
+                    border: 'dashed',
+                    borderColor: 'black'
+                    }}>
+                    Borrower's Information
+                </header>
+                <div style={{display:'inline-grid'}}>
+                    <input 
+                        style={{textAlign:'center', width:'400px', margin: '10px'}}
+                        type='text'
+                        placeholder='NAME'/>
+                    <input 
+                        style={{textAlign:'center', width:'400px', margin: '10px'}}
+                        type='number'
+                        placeholder='PHONE NUMBER'/>
+                </div>
+            </div>
+
+            <div>
+                <header style={{
+                    backgroundColor: 'gray',
+                    padding: '5px',
+                    border: 'dashed',
+                    borderColor: 'black'
+                    }}>
+                    Borrowing Details
+                </header>
+                <div style={{display:'inline-grid'}}>
+                    <p style={{textAlign:'center', width:'400px', margin: '5px'}}>
+                        RECEIPT ID:
+                    </p>
+                    <input 
+                        style={{textAlign:'center', width:'400px', margin: '5px'}}
+                        type='text'
+                        placeholder='BOOK'/>
+                    <input 
+                        style={{textAlign:'center', width:'400px', margin: '5px', fontSize:'15px'}}
+                        type='date'
+                        placeholder='BORROWING DATE'/>
+                    <p style={{textAlign:'center', width:'400px', margin: '5px', fontSize:'15px'}}>
+                        DUE DATE:
+                    </p>
+                    <input 
+                        style={{alignContent:'center', width:'100px', margin: '5px', fontSize:'15px', padding: '5px'
+                                , backgroundColor:'violet', cursor:'pointer'}}
+                        type='submit'
+                        />
+                </div>
+            </div>
+        </div>
+    )
 }
